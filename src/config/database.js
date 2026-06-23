c359acb3a1e3da33371367b7262fbc25a6937f1b
yJeanx11x <j3anlucas010@gmail.com>
2026-06-23 15:30:10 -0300

    fix: corrige a inicialização do Sequelize e a autenticação da conexão com o banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
-const sequelize = require('sequelize')
+const Sequelize = require('sequelize')
 
-const Sequelize = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT
 })
 
 
 try {
-    Sequelize.authenticate()
+    sequelize.authenticate()
     console.log('A conexão foi estabelecida com sucesso.')
 } catch (error) {
     console.log('Não foi possível conectar ao banco de dados:', erro)
@@ -16,6 +16,6 @@ try {
 }
 
 module.exports={
-    sequelize:sequelize,
-    Sequelize:Sequelize
+    sequelize,
+    Sequelize,
 }
