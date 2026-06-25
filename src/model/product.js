aa1fd147bc8eeb388bd5f54049059adf06204fdc
yJeanx11x <j3anlucas010@gmail.com>
2026-06-25 16:57:44 -0300

    feat: adiciona modelo de produto ao banco de dados

---
+++ src/model/product.js
@@ -0,0 +1,23 @@
+const db = require('../config/database')
+const Product = db.sequelize.define('Product', {
+    nome: {
+        type: db.Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    descricao: {
+        type: db.Sequelize.STRING,
+        allowNull: false,
+    },
+    preco: {
+        type: db.Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+
+    },
+    estoque: {
+        type: db.Sequelize.STRING,
+        allowNull: false,
+    }
+})
+
+module.exports = { Product }
