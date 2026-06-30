195a525d9736c2abd666c230d22c74206aed74bd
yJeanx11x <j3anlucas010@gmail.com>
2026-06-30 16:34:06 -0300

    feat: adiciona verificação de acesso para criação de produtos e corrige formatação do código

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,11 +1,12 @@
 
 const { Product } = require('../model/product')
+const { User } = require('../model/user')
 
 async function produto(req, res, next) {
 
 
     try {
-        const product = await Product.findAll({ attributes: ['id','nome', 'preco', 'estoque'] })
+        const product = await Product.findAll({ attributes: ['id', 'nome', 'preco', 'estoque'] })
 
         if (product.length === 0) {
             return res.status(401).json({ message: 'Nenhum produto cadastrado.' })
@@ -20,23 +21,27 @@ async function produto(req, res, next) {
 async function criarProduto(req, res, next) {
     const { nome, descricao, preco, estoque } = req.body
     try {
+        if (req.user.role !== 'admin') {
+           return res.status(401).json({ message: 'Acesso negado. Somente administradores.' })
+       }
 
         const nomeProduto = await Product.findOne({ where: { nome } })
         if (nomeProduto) {
             return res.status(409).json({ message: 'Já existe um produto com este nome.' })
         }
-
         await Product.create({
             nome,
             descricao,
             preco,
-            estoque
-
+            estoque,
+            userId: req.user.id 
         })
 
         return res.status(201).json({ message: 'Produto cadastrado com sucesso.' })
+
+
     } catch (error) {
-        next()
+        next(error)
     }
 
 }
