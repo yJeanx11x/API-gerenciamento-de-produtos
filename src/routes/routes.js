2252a6c4a6bca45112f1afc50533a25695de40ea
yJeanx11x <j3anlucas010@gmail.com>
2026-07-01 16:06:46 -0300

    feat: adiciona verificação de acesso de administrador para criação e deleção de produtos

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,14 +5,16 @@ const jwt = require('../middlewares/jwt')
 
 const produtos = require('../controllers/productController');
 
+const admin = require('../middlewares/isAdmin')
+
 appRoute.post('/register', authcontroll.registe)
 appRoute.post('/login', authcontroll.login)
 
 appRoute.get('/produto', jwt.verificarToken, produtos.produto)
 
 // função do adm criar / deletar / atualizar 
-appRoute.post('/produto',jwt.verificarToken, produtos.criarProduto)
+appRoute.post('/produto', jwt.verificarToken,admin.isAdmin ,produtos.criarProduto)
 
-appRoute.delete('/produto/:id',jwt.verificarToken, produtos.deltarProduto)
+appRoute.delete('/produto/:id', jwt.verificarToken, admin.isAdmin, produtos.deltarProduto)
 
 module.exports = appRoute
