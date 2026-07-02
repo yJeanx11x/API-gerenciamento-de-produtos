9422b695319b31e4fc6afde08c9b4752d3dcff46
yJeanx11x <j3anlucas010@gmail.com>
2026-07-02 15:32:09 -0300

    feat: adiciona relacionamento entre usuários e pedidos no modelo de usuário

--- src/model/user.js
+++ src/model/user.js
@@ -1,5 +1,6 @@
 const db = require('../config/database')
 const { Product } = require('../model/product')
+const { order } = require('../model/order')
 
 const User = db.sequelize.define('User', {
     nome: {
@@ -28,11 +29,15 @@ const User = db.sequelize.define('User', {
 User.hasMany(Product, { foreignKey: 'userId' })
 Product.belongsTo(User, { foreignKey: 'userId' })
 
+User.hasMany(order, { foreignKey: 'userId' })
+order.belongsTo(User, { foreignKey: 'userId' })
+
 
 async function dataBase() {
     try {
         await User.sync({ force: false })
         await Product.sync({ force: false })
+        await order.sync({ force: false })
         console.log('Tabelas criadas com sucesso')
     } catch (error) {
         console.log('erro na criação de tabela', error)
